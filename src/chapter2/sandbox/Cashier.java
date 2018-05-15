8944b178117d8fac698657264743b7470ad98d40
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-15 23:57:35 +0300

    Add Cashier

---
+++ src/chapter2/sandbox/Cashier.java
@@ -0,0 +1,15 @@
+/**
+ * Program to test how functional interfaces work.
+ *
+ * @author Elizabeth Berg
+ */
+package chapter2.sandbox;
+
+public class Cashier {
+
+    public void cashIn() {
+    }
+
+    public static void main(String[] args) {
+    }
+}
