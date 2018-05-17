19f40799db8f33d4a47bd47fed56e0a2eb9d6bde
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-17 23:02:01 +0300

    Add Question3

---
+++ src/chapter2/answers/Question3.java
@@ -0,0 +1,22 @@
+package chapter2.answers;
+
+import javax.swing.JButton;
+
+/** Type inference rules
+ *
+ * @author Elizabeth Berg, code in a and b is from the book
+ */
+public class Question3 {
+
+    public static void main(String[] args) {
+        // a is okay
+        Runnable helloWorld = () -> System.out.println("hello world");
+        helloWorld.run();
+
+        // b is okay
+        JButton button = new JButton();
+        button.addActionListener(event ->
+            System.out.println(event.getActionCommand()));
+    }
+
+}
