cc6f674db8a30ec641786c54e149a605080f22db
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-20 20:21:31 +0300

    chapter 3, question 1: Supplement addByExternal_Iteration

--- src/chapter3/answers/Question1.java
+++ src/chapter3/answers/Question1.java
@@ -1,5 +1,9 @@
 package chapter3.answers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Stream;
 
 /** Three ways to add up numbers.
@@ -8,7 +12,7 @@ import java.util.stream.Stream;
  */
 public class Question1 {
     // 1. The traditional approach using a for loop
-    public static int addUpByLooping(int[] allNumbers) {
+    public static int addByLooping(Integer[] allNumbers) {
         int sum = 0;
         for (int number : allNumbers) {
             sum += number;
@@ -17,16 +21,30 @@ public class Question1 {
     }
 
     // 2. External iteration
-    public static int addUpByExternal_Iteration() {
-        return 0;
+    public static int addByExternal_Iteration(List<Integer> allNumbers) {
+        int sum = 0;
+        Iterator<Integer> iterator = allNumbers.iterator();
+        while (iterator.hasNext()) {
+            sum += iterator.next();
+        }
+        return sum;
     }
     // 3. Internal iteration using Streams
-    public static int addUpByInternal_iteration(Stream<Integer> numbers) {
+    public static int addByInternal_iteration(Stream<Integer> numbers) {
         return 0;
     }
 
     public static void main(String[] args) {
-        int[] numbers2 = {1, 2, 3, 4, 5};
+        // 1. Method
+        Integer[] numbers = new Integer[] {1, 2, 3, 4, 5};
+        System.out.println(addByLooping(numbers));
+
+        // 2. Method
+        List<Integer> numbersList = new ArrayList<>();
+        numbersList.addAll(Arrays.asList(numbers));
+        System.out.println(addByExternal_Iteration(numbersList));
+
+        // 3. Method
 
     }
 }
