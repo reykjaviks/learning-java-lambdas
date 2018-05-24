f961f95875dc8995c97834b7baf2f8fd0653aea4
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-25 00:24:01 +0300

    Add chapter 3, question 2

---
+++ src/chapter3/answers/Question2.java
@@ -0,0 +1,35 @@
+package chapter3.answers;
+
+import common.Artist;
+import static java.util.Arrays.asList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/** Refactor external_Iteration to use internal iteration.
+ *
+ * @author Elizabeth Berg
+ */
+public class Question2 {
+    public static int external_Iteration(List<Artist> artists) {
+        int totalMembers = 0;
+        for (Artist artist : artists) {
+            Stream<Artist> members = artist.getMembers();
+            totalMembers += members.count();
+        }
+        return totalMembers;
+    }
+
+    public static Long internal_Iteration(List<Artist> artists) {
+        return artists.stream()
+                .flatMap(artist -> artist.getMembers())
+                .count();
+    }
+
+    public static void main(String[] args) {
+        Artist Sonoio = new Artist("Sonoio", "Los Angeles, California");
+        Artist Alessandro = new Artist("Alessandro Cortini", "Los Angeles, California");
+        Sonoio.addMembers(Alessandro);
+        List<Artist> artists = asList(Sonoio);
+    }
+
+}
--- src/common/Artist.java
+++ src/common/Artist.java
@@ -1,5 +1,9 @@
 package common;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  *
  * @author Elizabeth Berg
@@ -7,14 +11,22 @@ package common;
 public class Artist {
     private String name;
     private String origin;
+    private List<Artist> members;
     
     public Artist(String name, String origin) {
         this.name = name;
         this.origin = origin;
+        this.members = new ArrayList<>();
+    }
+
+    public void addMembers(Artist artist) {
+        this.members.add(artist);
     }
 
     public String getName() {
         return this.name;
     }
-
+    public Stream<Artist> getMembers() {
+        return this.members.stream();
+    }
 }
