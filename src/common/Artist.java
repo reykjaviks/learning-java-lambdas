7ed5b51e79681fe2b949b93be24198f1ea4fb0d0
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-24 23:17:17 +0300

    Add common package and Artist

---
+++ src/common/Artist.java
@@ -0,0 +1,20 @@
+package common;
+
+/**
+ *
+ * @author Elizabeth Berg
+ */
+public class Artist {
+    private String name;
+    private String origin;
+    
+    public Artist(String name, String origin) {
+        this.name = name;
+        this.origin = origin;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
