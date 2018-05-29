26d316683dc00e2f4bf3fdeebe2936124c9d93b7
Elizabeth Berg <elizabethinposti@gmail.com>
2018-05-29 23:52:56 +0300

    Add chapter 4, sandbox: LambdaLogging

---
+++ src/chapter4/sandbox/LambdaLogging.java
@@ -0,0 +1,13 @@
+package chapter4.sandbox;
+
+/**
+ *
+ * @author Elizabeth Berg
+ */
+public class LambdaLogging {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
