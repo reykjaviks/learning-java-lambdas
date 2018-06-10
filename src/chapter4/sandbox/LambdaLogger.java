a6e2f3bfb330ab8458cafdb0c13ffedec2f3d00c
Elizabeth Berg <elizabethinposti@gmail.com>
2018-06-10 22:22:04 +0300

    Play with log4j

--- src/chapter4/sandbox/LambdaLogger.java
+++ src/chapter4/sandbox/LambdaLogger.java
@@ -1,13 +1,28 @@
 package chapter4.sandbox;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author Elizabeth Berg
  */
-public class LambdaLogging {
+public class LambdaLogger {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    final static Logger logger = Logger.getLogger(LambdaLogger.class);
+
+    // Traditional way of optimizing logger
+    public void changeName(String name) {
+        BasicConfigurator.configure();
+        if (logger.isDebugEnabled()) { // Not needed if message does not involve concatenating
+            logger.debug("Save successful, new name: " + name);
+        }
     }
     
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        logger.debug("This is debug ");
+        logger.info("Entering the Execute method");
+        logger.error("Error while saving");
+    }
 }
