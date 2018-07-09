1ee3189cb36483c1c9470dd6f321daea1116367b
Elizabeth Berg <elizabethinposti@gmail.com>
2018-07-09 23:16:45 +0300

    Add Chapter 4, Question 1: Performance interface

--- src/common/Performance.java
+++ src/common/Performance.java
@@ -1,20 +1,28 @@
 package common;
 
+import java.util.List;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
 import static java.util.stream.Stream.concat;
 
+/* A Performance by some musicians - e.g., an Album or Gig */
 public interface Performance {
 
     public String getName();
 
     public Stream<Artist> getMusicians();
 
-    // TODO: test
-    public default Stream<Artist> getAllMusicians() {
-        return getMusicians().flatMap(artist -> {
-            return concat(Stream.of(artist), artist.getMembers());
-        });
+    public default List<String> getAllMusicians(Artist artist) {
+        return artist.getMembers()
+                .map(member -> member.getName())
+                .collect(toList());
+    }
+
+    public static List<String> findNames(List<old_common.Artist> artists) {
+        return artists.stream()
+                .map(artist -> artist.getName())
+                .collect(toList());
     }
 
 }
