904d78cecfb2d0446a03f353fcdbd250b24cf0b1
Elizabeth Berg <elizabethinposti@gmail.com>
2018-07-29 20:50:07 +0300

    Indent Foo

--- src/chapter4/sandbox/Foo.java
+++ src/chapter4/sandbox/Foo.java
@@ -1,31 +1,31 @@
 package chapter4.sandbox;
 
+import java.util.Arrays;
 import old_common.Album;
 import old_common.Artist;
 import old_common.InitArtists;
 import java.util.IntSummaryStatistics;
-import java.util.List;
 
 /**
  *
  * @author Elizabeth Berg
  */
 public class Foo {
-    
-    public static void printTrackLengthStatistics(Album album) {
-    IntSummaryStatistics trackLengthStats
-            = album.getTracks()
-                   .mapToInt(track -> track.getLength())
-                   .summaryStatistics();
 
-    System.out.printf("Max: %d, Min: %d, Ave: %f, Sum: %d",
-                      trackLengthStats.getMax(),
-                      trackLengthStats.getMin(),
-                      trackLengthStats.getAverage(),
-                      trackLengthStats.getSum());
-}
+    public static void printTrackLengthStatistics(Album album) {
+        IntSummaryStatistics trackLengthStats
+                = album.getTracks()
+                .mapToInt(track -> track.getLength())
+                .summaryStatistics();
+
+        System.out.printf("Max: %d, Min: %d, Ave: %f, Sum: %d",
+                trackLengthStats.getMax(),
+                trackLengthStats.getMin(),
+                trackLengthStats.getAverage(),
+                trackLengthStats.getSum());
+    }
 
     public static void main(String[] args) {
     }
-    
+
 }
